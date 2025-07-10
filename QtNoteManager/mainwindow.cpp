b6cf13a86e7397e28a80992b123eef1a413daad6
dianxqwidi <dianaradomska4@gmail.com>
2025-07-10 19:30:59 +0300

    created MainWindow class, UI setup, basic slots and connections

--- QtNoteManager/mainwindow.cpp
+++ QtNoteManager/mainwindow.cpp
@@ -1,14 +1,73 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QFile>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    networkManager = new QNetworkAccessManager(this);
+
+    connect(ui->folderList, &QListWidget::currentRowChanged, this, &MainWindow::onFolderSelectionChanged);
+    connect(ui->noteList, &QListWidget::currentRowChanged, this, &MainWindow::onNoteSelectionChanged);
+
+    loadData();
+    updateFolderList();
 }
 
 MainWindow::~MainWindow()
 {
+    saveData();
     delete ui;
+}
+
+void MainWindow::updateFolderList() {
+    ui->folderList->clear();
+    for (const Folder &folder : folders)
+        ui->folderList->addItem(folder.name);
+    if (!folders.isEmpty()) {
+        currentFolderIndex = qMin(currentFolderIndex, folders.size() - 1);
+        ui->folderList->setCurrentRow(currentFolderIndex);
+    }
+}
+
+void MainWindow::updateNoteList() {
+    ui->noteList->clear();
+    if (currentFolderIndex < 0 || currentFolderIndex >= folders.size()) return;
+    const Folder &folder = folders[currentFolderIndex];
+    for (const Note &note : folder.notes)
+        ui->noteList->addItem(note.title);
+    if (!folder.notes.isEmpty()) {
+        currentNoteIndex = qMin(currentNoteIndex, folder.notes.size() - 1);
+        ui->noteList->setCurrentRow(currentNoteIndex);
+    }
+}
+
+void MainWindow::updateNoteEditor() {
+    if (currentFolderIndex < 0 || currentFolderIndex >= folders.size() ||
+        currentNoteIndex < 0 || currentNoteIndex >= folders[currentFolderIndex].notes.size()) {
+        ui->noteEditor->clear();
+        return;
+    }
+    ui->noteEditor->setPlainText(folders[currentFolderIndex].notes[currentNoteIndex].content);
+}
+
+void MainWindow::onFolderSelectionChanged() {
+    currentFolderIndex = ui->folderList->currentRow();
+    currentNoteIndex = -1;
+    updateNoteList();
+    updateNoteEditor();
+}
+
+void MainWindow::onNoteSelectionChanged() {
+    currentNoteIndex = ui->noteList->currentRow();
+    updateNoteEditor();
 }
--- QtNoteManager/mainwindow.h
+++ QtNoteManager/mainwindow.h
@@ -2,11 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+#include <QString>
+#include <QNetworkAccessManager>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +18,32 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onFolderSelectionChanged();
+    void onNoteSelectionChanged();
+
 private:
     Ui::MainWindow *ui;
+
+    struct Note {
+        QString title;
+        QString content;
+    };
+
+    struct Folder {
+        QString name;
+        QList<Note> notes;
+    };
+
+    QList<Folder> folders;
+    int currentFolderIndex = -1;
+    int currentNoteIndex = -1;
+
+    void updateFolderList();
+    void updateNoteList();
+    void updateNoteEditor();
+
+    QNetworkAccessManager *networkManager;
 };
+
 #endif // MAINWINDOW_H
